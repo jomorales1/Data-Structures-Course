87f21002977057e3e9454724799bbcf62d458461
Jorge Aurelio Morales Manrique <manriquem1901@gmail.com>
2020-09-15 21:11:25 -0500

    Week 1 - Second problem solved!

--- course2_2003291203/week1_basic_data_structures/2_tree_height/tree-height.cpp
+++ course2_2003291203/week1_basic_data_structures/2_tree_height/tree-height.cpp
@@ -1,79 +1,49 @@
-#include <algorithm>
-#include <iostream>
-#include <vector>
-#if defined(__unix__) || defined(__APPLE__)
-#include <sys/resource.h>
-#endif
+#include <bits/stdc++.h>
 
-class Node;
+using namespace std;
 
-class Node {
-public:
-    int key;
-    Node *parent;
-    std::vector<Node *> children;
-
-    Node() {
-      this->parent = NULL;
-    }
-
-    void setParent(Node *theParent) {
-      parent = theParent;
-      parent->children.push_back(this);
-    }
+template <class genType>
+class node {
+    public:
+        genType key;
+        vector<node<genType>*> children;
+        node(genType key) {
+            this->key = key;
+        }
+        void add_children(node<genType>* child) {
+            this->children.push_back(child);
+        }
+        int number_children() {
+            return this->children.size();
+        }
 };
 
-
-int main_with_large_stack_space() {
-  std::ios_base::sync_with_stdio(0);
-  int n;
-  std::cin >> n;
-
-  std::vector<Node> nodes;
-  nodes.resize(n);
-  for (int child_index = 0; child_index < n; child_index++) {
-    int parent_index;
-    std::cin >> parent_index;
-    if (parent_index >= 0)
-      nodes[child_index].setParent(&nodes[parent_index]);
-    nodes[child_index].key = child_index;
-  }
-
-  // Replace this code with a faster implementation
-  int maxHeight = 0;
-  for (int leaf_index = 0; leaf_index < n; leaf_index++) {
-    int height = 0;
-    for (Node *v = &nodes[leaf_index]; v != NULL; v = v->parent)
-      height++;
-    maxHeight = std::max(maxHeight, height);
-  }
-    
-  std::cout << maxHeight << std::endl;
-  return 0;
+long long height(node<int>* tree) {
+    if (tree->number_children() == 0)
+        return 1;
+    long long maximum = 0LL;
+    for (auto &child : tree->children) {
+        maximum = max(maximum, height(child));
+    }
+    return (1LL + maximum);
 }
 
-int main (int argc, char **argv)
-{
-#if defined(__unix__) || defined(__APPLE__)
-  // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
-  struct rlimit rl;
-  int result;
-
-  result = getrlimit(RLIMIT_STACK, &rl);
-  if (result == 0)
-  {
-      if (rl.rlim_cur < kStackSize)
-      {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
-      }
-  }
-
-#endif
-  return main_with_large_stack_space();
+int main() {
+    int n; cin >> n;
+    vector<node<int>> nodes;
+    int root, parent_index;
+    for (int i = 0; i < n; i++) {
+        node<int> new_node(i);
+        nodes.push_back(new_node);
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> parent_index;
+        if (parent_index == -1) {
+            root = i;
+        } else {
+            nodes[parent_index].add_children(&nodes[i]);
+        }
+    }
+    cout << height(&nodes[root]) << '\n';
+    return 0;
 }
