44e0a57c1d69d2c6c9f4c6532f0bf37c87683474
Jorge Morales <jomorales@unal.edu.co>
2021-01-09 19:52:32 -0500

    Some corrections to the DynamicArray implementation.

--- Java Implementations/src/main/java/com/datastructures/dynamicarray/DynamicArray.java
+++ Java Implementations/src/main/java/com/datastructures/dynamicarray/DynamicArray.java
@@ -1,55 +1,67 @@
 package com.datastructures.dynamicarray;
 
+import com.datastructures.exception.EmptyStructureException;
+
 public class DynamicArray<T> {
-    private int size;
+    private int totalSize;
     private int capacity;
     private T[] elements;
 
     public DynamicArray(int size) {
-        this.size = size;
-        this.capacity++;
-        this.elements = (T[]) new Object[size];
+        this.totalSize = size;
+        this.capacity = 0;
+        this.elements = (T[]) new Object[totalSize];
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= this.capacity)
+            throw new ArrayIndexOutOfBoundsException();
+        return this.elements[index];
     }
 
     public void pushBack(T element) {
-        this.capacity++;
-        if (this.capacity == this.size - 1) {
-            T[] newArray = (T[]) new Object[this.size * 2];
-            for (int i = 0; i < capacity - 1; i++) {
+        if (this.capacity == this.totalSize - 1) {
+            T[] newArray = (T[]) new Object[this.totalSize * 2];
+            for (int i = 0; i < this.capacity; i++) {
                 newArray[i] = this.elements[i];
             }
-            newArray[capacity - 1] = element;
+            newArray[this.capacity] = element;
             this.elements = newArray;
-            this.size = this.size * 2;
+            this.totalSize = this.totalSize * 2;
         } else {
-            elements[this.capacity - 1] = element;
+            this.elements[this.capacity] = element;
         }
+        this.capacity++;
     }
 
-    public T popBack() {
-        if (capacity == 0) {
-            return null;
-        }
-        capacity--;
-        return elements[this.capacity];
+    public T popBack() throws EmptyStructureException {
+        if (this.capacity == 0)
+            throw new EmptyStructureException();
+        T value = this.elements[this.capacity - 1];
+        this.elements[this.capacity - 1] = null;
+        this.capacity--;
+        return value;
+    }
+
+    public void makeEmpty() {
+        this.elements = (T[]) new Object[this.totalSize];
     }
 
     public int getCapacity() {
-        return this.capacity - 1;
+        return this.capacity;
     }
 
-    public int getSize() {
-        return this.size;
+    public int getTotalSize() {
+        return this.totalSize;
     }
 
     @Override
     public String toString() {
-        if (this.capacity == 1)
-            return "";
+        if (this.capacity == 0)
+            return "[]";
         StringBuilder res = new StringBuilder("[");
         for (int i = 0; i < this.capacity - 1; i++) {
-            if (this.elements[i] != null)
-                res.append(this.elements[i]).append(", ");
+            res.append(this.elements[i]).append(", ");
         }
         res.append(this.elements[this.capacity - 1]).append("]");
         return res.toString();
