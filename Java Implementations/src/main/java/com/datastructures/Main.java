502908e5da9b665d6b761a796842578dc3003a64
Jorge Morales <jomorales@unal.edu.co>
2021-01-10 12:59:35 -0500

    Stack implementation using DoublyLinkedList finished.

--- Java Implementations/src/main/java/com/datastructures/Main.java
+++ Java Implementations/src/main/java/com/datastructures/Main.java
@@ -3,20 +3,52 @@ package com.datastructures;
 import com.datastructures.dynamicarray.DynamicArray;
 import com.datastructures.exception.ElementNotFoundException;
 import com.datastructures.exception.EmptyStructureException;
+import com.datastructures.linkedlist.DoublyLinkedList;
 import com.datastructures.linkedlist.SinglyLinkedList;
 import com.datastructures.stack.StackA;
+import com.datastructures.stack.StackLL;
 
 public class Main {
 
     public static void main(String[] args) throws EmptyStructureException, ElementNotFoundException {
         System.out.println("Hello World!");
         testSinglyLinkedList();
+        testDoublyLinkedList();
         testDynamicArray();
         testStackA();
+        testStackLL();
     }
 
     public static void testSinglyLinkedList() throws EmptyStructureException, ElementNotFoundException {
         SinglyLinkedList<Integer> list = new SinglyLinkedList<>();
+        System.out.println(list.toString());
+        for (int i = 0; i < 11; i++) {
+            list.pushBack(i + 1);
+            System.out.println(list.toString());
+        }
+        System.out.println("Erasing 5...");
+        list.erase(5);
+        System.out.println("Result: " + list.toString());
+        System.out.println("Last element: " + list.topBack());
+        System.out.println("Deleting last element...");
+        list.popBack();
+        System.out.println("Result: " + list.toString());
+        list.makeEmpty();
+        System.out.println(list.toString());
+        for (int i = 0; i < 11; i++) {
+            list.pushFront(i + 1);
+            System.out.println(list.toString());
+        }
+        System.out.println("First element: " + list.topFront());
+        System.out.println("Deleting first element...");
+        list.popFront();
+        System.out.println("Result: " + list.toString());
+        System.out.println("Size: " + list.getSize());
+        list.makeEmpty();
+    }
+
+    public static void testDoublyLinkedList() throws EmptyStructureException, ElementNotFoundException {
+        DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
         System.out.println(list.toString());
         for (int i = 0; i < 11; i++) {
             list.pushBack(i + 1);
@@ -60,6 +92,20 @@ public class Main {
 
     public static void testStackA() throws EmptyStructureException {
         StackA<Integer> stack = new StackA<>();
+        System.out.println(stack);
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+            System.out.println(stack);
+        }
+        System.out.println(stack.top());
+        for (int i = 0; i < 10; i++) {
+            stack.pop();
+            System.out.println(stack);
+        }
+    }
+
+    public static void testStackLL() throws EmptyStructureException {
+        StackLL<Integer> stack = new StackLL<>();
         System.out.println(stack);
         for (int i = 0; i < 10; i++) {
             stack.push(i);
--- Java Implementations/src/main/java/com/datastructures/stack/StackLL.java
+++ Java Implementations/src/main/java/com/datastructures/stack/StackLL.java
@@ -1,4 +1,50 @@
 package com.datastructures.stack;
 
-public class StackLL {
+import com.datastructures.exception.EmptyStructureException;
+import com.datastructures.linkedlist.DoublyLinkedList;
+
+public class StackLL<T> {
+    private int size;
+    private final DoublyLinkedList<T> elements;
+
+    public StackLL() {
+        this.size = 0;
+        this.elements = new DoublyLinkedList<>();
+    }
+
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    public void push(T key) {
+        this.elements.pushFront(key);
+        this.size++;
+    }
+
+    public T pop() throws EmptyStructureException {
+        T key = this.elements.topFront();
+        this.elements.popFront();
+        this.size--;
+        return key;
+    }
+
+    public T top() throws EmptyStructureException {
+        return this.elements.topFront();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Top -> ");
+        for (int i = 0; i < this.size - 1; i++) {
+            stringBuilder.append("[").append(this.elements.get(i)).append("] ");
+        }
+        stringBuilder.append("[").append(this.elements.get(this.size - 1)).append("]");
+        return stringBuilder.toString();
+    }
 }
