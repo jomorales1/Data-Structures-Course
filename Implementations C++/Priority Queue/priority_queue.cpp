8df233db9790eebf920e0b3cba179e4ce729de2b
Jorge Morales <jomorales@unal.edu.co>
2021-01-29 17:56:51 -0500

    MinPriorityQueue implementation in C++.

--- Implementations C++/Priority Queue/priority_queue.cpp
+++ Implementations C++/Priority Queue/priority_queue.cpp
@@ -2,10 +2,12 @@
 
 using namespace std;
 
+#define NEG(x) (-(x))
+
 template <class genType, int msize = 50>
-class priority_queue_t {
+class max_priority_queue_t {
     public:
-        priority_queue_t() {
+        max_priority_queue_t() {
             this->elements = new genType[msize + 1];
         }
         void insert(genType value) {
@@ -98,8 +100,109 @@ class priority_queue_t {
         }
 };
 
+template <class genType, int msize = 50>
+class min_priority_queue_t {
+    public:
+        min_priority_queue_t() {
+            this->elements = new genType[msize + 1];
+        }
+        void insert(genType value) {
+            if (this->size == this->max_size) {
+                cout << "Not enough space.\n";
+                return;
+            }
+            this->size++;
+            this->elements[this->size] = NEG(value);
+            sift_up(this->size);
+        }
+        genType extract_min() {
+            if (this->size == 0) {
+                cout << "Empty heap.\n";
+                return (genType) NULL;
+            }
+            genType result = this->elements[1];
+            this->elements[1] = this->elements[this->size];
+            this->size--;
+            sift_down(1);
+            return (genType) NEG(result);
+        }
+        void remove(int index) {
+            if (typeid(genType) == typeid(int)) {
+                this->elements[index] = INT_MAX;
+            } else if (typeid(genType) == typeid(double)) {
+                this->elements[index] = DBL_MAX;
+            } else if (typeid(genType) == typeid(long long)) {
+                this->elements[index] = __LONG_LONG_MAX__;
+            } else {
+                cout << "Data type not supported.\n";
+                return;
+            }
+            sift_up(index);
+            extract_min();
+        }
+        void change_priority(int index, genType priority) {
+            genType old_priority = this->elements[index];
+            this->elements[index] = NEG(priority);
+            if (priority > old_priority) {
+                sift_up(index);
+            } else {
+                sift_down(index);
+            }
+        }
+        string to_string_t() {
+            string res = "[";
+            for (int i = 1; i <= this->size; i++) {
+                res += to_string(NEG(this->elements[i]));
+                if (i != this->size) {
+                    res += ", ";
+                } else {
+                    res += "]\n";
+                }
+            }
+            return res;
+        }
+    private:
+        int size = 0;
+        int max_size = msize;
+        genType* elements;
+
+        int parent(int index) {
+            return index / 2;
+        }
+        int left_child(int index) {
+            return 2 * index;
+        }
+        int right_child(int index) {
+            return 2 * index + 1;
+        }
+        void sift_up(int index) {
+            while (index > 1 && this->elements[parent(index)] < this->elements[index]) {
+                swap(this->elements[parent(index)], this->elements[index]);
+                index = parent(index);
+            }
+        }
+        void sift_down(int index) {
+            int max_index = index;
+            int left = left_child(index);
+            if (left <= this->size && this->elements[left] > this->elements[max_index])
+                max_index = left;
+            int right = right_child(index);
+            if (right <= this->size && this->elements[right] > this->elements[max_index])
+                max_index = right;
+            if (index != max_index) {
+                swap(this->elements[index], this->elements[max_index]);
+                sift_down(max_index);
+            }
+        }
+};
+
+void heap_sort(vector<int> &elements) {
+    
+}
+
 int main() {
-    priority_queue_t<int, 20> queue;
+    cout << "Max Priority Queue:\n";
+    max_priority_queue_t<int, 20> queue;
     for (int i = 10; i >= 5; i--) {
         queue.insert(i - 5);
         queue.insert(i);
@@ -109,5 +212,16 @@ int main() {
     cout << queue.to_string_t();
     cout << to_string(queue.extract_max()) << '\n';
     cout << queue.to_string_t();
+    cout << "\nMin Priority Queue:\n";
+    min_priority_queue_t<int, 20> queue2;
+    for (int i = 10; i >= 5; i--) {
+        queue2.insert(i - 5);
+        queue2.insert(i);
+    }
+    cout << queue2.to_string_t();
+    queue2.remove(3);
+    cout << queue2.to_string_t();
+    cout << to_string(queue2.extract_min()) << '\n';
+    cout << queue2.to_string_t();
     return 0;
 }
