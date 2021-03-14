76ac290d6725cdc524e3c9a02611ce08e0d8ea15
Jorge Morales <jomorales@unal.edu.co>
2021-03-14 16:13:04 -0500

    Week 4 - First problem solved

--- course2_2003291203/week3_hash_tables/1_phone_book/phone_book.cpp
+++ course2_2003291203/week3_hash_tables/1_phone_book/phone_book.cpp
@@ -1,72 +1,32 @@
-#include <iostream>
-#include <vector>
-#include <string>
+#include <bits/stdc++.h>
 
-using std::string;
-using std::vector;
-using std::cin;
+using namespace std;
 
-struct Query {
-    string type, name;
-    int number;
-};
-
-vector<Query> read_queries() {
-    int n;
-    cin >> n;
-    vector<Query> queries(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> queries[i].type;
-        if (queries[i].type == "add")
-            cin >> queries[i].number >> queries[i].name;
-        else
-            cin >> queries[i].number;
-    }
-    return queries;
-}
-
-void write_responses(const vector<string>& result) {
-    for (size_t i = 0; i < result.size(); ++i)
-        std::cout << result[i] << "\n";
-}
-
-vector<string> process_queries(const vector<Query>& queries) {
-    vector<string> result;
-    // Keep list of all existing (i.e. not deleted yet) contacts.
-    vector<Query> contacts;
-    for (size_t i = 0; i < queries.size(); ++i)
-        if (queries[i].type == "add") {
-            bool was_founded = false;
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts[j].name = queries[i].name;
-                    was_founded = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!was_founded)
-                contacts.push_back(queries[i]);
-        } else if (queries[i].type == "del") {
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    contacts.erase(contacts.begin() + j);
-                    break;
-                }
-        } else {
-            string response = "not found";
-            for (size_t j = 0; j < contacts.size(); ++j)
-                if (contacts[j].number == queries[i].number) {
-                    response = contacts[j].name;
-                    break;
-                }
-            result.push_back(response);
-        }
-    return result;
-}
+const int N = 1e8;
+unordered_map<int, string> phone_book;
 
 int main() {
-    write_responses(process_queries(read_queries()));
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    int n = 0; cin >> n;
+    for (int i = 0; i < n; i++) {
+        string action; cin >> action;
+        if (action == "add") {
+            int number; cin >> number;
+            string name; cin >> name;
+            phone_book[number] = name;
+        } else if (action == "del") {
+            int number; cin >> number;
+            if (phone_book.count(number))
+                phone_book.erase(number);
+        } else {
+            int number; cin >> number;
+            if (phone_book.count(number)) {
+                cout << phone_book[number] << '\n';
+            } else {
+                cout << "not found\n";
+            }
+        }
+    }
     return 0;
 }
