19bbde19629990111189456c8d5e527f0b1566b8
Jorge Morales <jomorales@unal.edu.co>
2021-04-24 19:03:46 -0500

    Week 4 - Third problem solved

--- course2_2003291203/week3_hash_tables/3_hash_substring/hash_substring.cpp
+++ course2_2003291203/week3_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,38 +1,67 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
+#include <cstdlib>
 
-using std::string;
-typedef unsigned long long ull;
+using namespace std;
 
-struct Data {
-    string pattern, text;
-};
+const long long p = 1e9 + 7;
 
-Data read_input() {
-    Data data;
-    std::cin >> data.pattern >> data.text;
-    return data;
+long long poly_hash(string &s, int left, int right, long long x) {
+    long long hash = 0LL;
+    int n = (int) s.length();
+    for (int i = right; i >= left; i--) {
+        hash = (hash * x + (long long) s[i]) % p;
+    }
+    return hash;
 }
 
-void print_occurrences(const std::vector<int>& output) {
-    for (size_t i = 0; i < output.size(); ++i)
-        std::cout << output[i] << " ";
-    std::cout << "\n";
+vector<long long> precompute_hashes(string &T, int pattern_len, long long x) {
+    int len = (int) T.length();
+    int n = len - pattern_len + 1;
+    vector<long long> hashes(n);
+    hashes[n - 1] = poly_hash(T, n - 1, len - 1, x);
+    long long y = 1LL;
+    for (int i = 1; i <= pattern_len; i++) {
+        y = (y * x) % p;
+    }
+    for (int i = n - 2; i >= 0; i--) {
+        hashes[i] = ((x * hashes[i + 1]) + (long long) T[i] - y * ((long long) T[i + pattern_len])) % p;
+    }
+    return hashes;
 }
 
-std::vector<int> get_occurrences(const Data& input) {
-    const string& s = input.pattern, t = input.text;
-    std::vector<int> ans;
-    for (size_t i = 0; i + s.size() <= t.size(); ++i)
-        if (t.substr(i, s.size()) == s)
-            ans.push_back(i);
-    return ans;
+bool are_equal(string &T, int start, string &S) {
+    for (int i = 0; i < (int) S.length(); i++) {
+        if (T[start + i] != S[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
+vector<int> rabin_karp(string &T, string &P) {
+    long long x = (long long) (rand() % (p - 1)) + 1;
+    vector<int> result;
+    long long p_hash = poly_hash(P, 0, (int) P.length(), x);
+    vector<long long> H = precompute_hashes(T, (int) P.length(), x);
+    int n = (int) H.size();
+    for (int i = 0; i < n; i++) {
+        if (p_hash != H[i]) {
+            continue;
+        }
+        if (are_equal(T, i, P)) {
+            result.push_back(i);
+        }
+    }
+    return result;
+}
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    print_occurrences(get_occurrences(read_input()));
+    string P; cin >> P;
+    string T; cin >> T;
+    vector<int> result = rabin_karp(T, P);
+    for (int pos : result) {
+        cout << pos << ' ';
+    }
+    cout << '\n';
     return 0;
 }
